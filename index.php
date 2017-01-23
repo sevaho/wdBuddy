65e153fb97e2dd13f22a0cd57489b26083d64b0b
sevaho <sebastiaan.vanhoecke@hotmail.be>
2017-01-23 19:10:31 +0100

    git quick push

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+  function consolelog($data){
+
+    if (is_array( $data))
+      $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data ) . "'  );</script>";
+    else
+      $output = "<script>console.log( 'Debug Objects: " . $data . "'  );</script>";
+
+    echo $output;
+  }
+
+  consolelog("Test");
+?>
